29a33cca249bd01c953a2cb35d2165d273963c2e
lixcx <lixcx@lixcxdemac.local>
2018-11-15 15:56:36 +0800

    Initial Commit

---
+++ lanYa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lanYa
+//
+//  Created by lixcx on 2018/11/15.
+//  Copyright © 2018 lixcx.sq. All rights reserved.
+//
+
+import UIKit
+import <#module#>
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
